fd3162fc796986d7f987081955fbc3d4c60b0c26
IagoBGCarvalho <iagobgc@gmail.com>
2025-09-02 15:56:59 -0300

    Finalização do curso de javascript, envolvendo a utilização de expressões regulares para filtragem e a utilização do fetch e de um arquivo json para buscar dados.

---
+++ 1 - HTML, CSS e Javascript/8 - Javascript/js/buscar-pacientes.js
@@ -0,0 +1,22 @@
+﻿var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function () {
+    console.log("Buscando pacientes...");
+    fetch("js/pacientes.json") // Gera uma promisse assíncrona que pode representar pendência, sucesso ou erro.
+        // Then é ativado caso a captura de dados for concluída com sucesso
+        .then(response => response.json()) // Responde é um objeto que representa a resposta HTTP, o response.josn() é um método que pega o copro da resposta e a trasnforma de uma string para um array Javascript. Gera outra promisse.
+        .then(pacientes => { // Caso a promisse do response.json seja bem sucedida, executa o processamento.
+            pacientes.forEach(function (paciente) {
+                var erroFetch = document.querySelector("#erro-fetch");
+                erroFetch.classList.add("invisivel");
+                var pacienteTr = montaTr(paciente); // Usa função do form.js
+                var tabela = document.querySelector("#tabela-pacientes");
+                tabela.appendChild(pacienteTr);
+            });
+        })
+        .catch(function (error) { // Catch é ativado caso a entrega de dados não seja concluída com sucesso
+            console.error("Erro ao buscar pacientes:", error);
+            var erroFetch = document.querySelector("#erro-fetch");
+            erroFetch.classList.remove("invisivel");
+        });
+});
--- 1 - HTML, CSS e Javascript/8 - Javascript/js/filtra.js
+++ 1 - HTML, CSS e Javascript/8 - Javascript/js/filtra.js
@@ -2,9 +2,21 @@
 
 campoFiltro.addEventListener("input", function () {
     var pacientes = document.querySelectorAll(".paciente");
-    for (var i = 0; i < pacientes.length; i++) {
-        var paciente = pacientes[i];
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = tdNome.textContent;
+    if (this.value.length > 0) { // Se o valor do campo de filtro for maior que 0, ou seja, se houver algo digitado no campo, adiciona a classe invisível aos pacientes que não correspondem ao filtro
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i"); // Cria uma expressão regular com o valor do campo de filtro. O "i" indica que a busca será feita ignorando maiúsculas e minúsculas
+            if (!expressao.test(nome)) { // Testa se o nome do paciente corresponde ao valor do campo de filtro, ignorando maiúsculas e minúsculas
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else { // Se o campo de filtro estiver vazio, remove a classe invisível de todos os pacientes
+        pacientes.forEach(function(paciente) {
+            paciente.classList.remove("invisivel");
+        });
     }
 });
