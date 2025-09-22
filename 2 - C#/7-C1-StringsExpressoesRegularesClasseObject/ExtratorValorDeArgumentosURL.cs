cdcfd01fbd0a9101b06faa27e7218e535aa06ac7
IagoBGCarvalho <iagobgc@gmail.com>
2025-09-22 17:54:31 -0300

    Uso de métodos String, como: ToUpper, ToLower, IndexOf, Substring, Remove, IsNullOrEmpty, StartsWith, EndsWith e Contains; e começo dos estudos de Expressões Regulares utilizando a classe Regex e seus métodos.

---
+++ 2 - C#/7-C1-StringsExpressoesRegularesClasseObject/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_C1_StringsExpressoesRegularesClasseObject
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get; }
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if(string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio. ", nameof(url));
+            } // Estas linha de código faz o que todas as linhas a seguir fariam:
+            #region
+            //if (url == null)
+            //{
+            //    throw new ArgumentNullException(nameof(url));
+            //}
+
+            //if (url.Length == 0)
+            //{
+            //    throw new ArgumentException("O argumento não pode ser vazio.", nameof(url));
+            //}
+            #endregion
+
+            int indiceInterrogacao = url.IndexOf('?'); // Faz uma busca na string e retorna o índice do caractere informado
+            _argumentos = url.Substring(indiceInterrogacao + 1); // Retorna uma parte da string solicitada. Recebe como parâmetro o primeiro índice da string que se vai recortar. Por receber o método IndexOf + 1 (1 caractere após o "?"), sempre vai recortar corretamente.
+
+            URL = url;
+        }
+
+        // moedaOrigem=real&moedaDestino=dolar
+        public string GetValor(string nomeParametro)
+        {
+            nomeParametro = nomeParametro.ToUpper(); // Método que trasnformar todos os caracteres e, caixa alta para evitar um erro na busca pelos índices
+            string argumentoEmCaixaAlta = _argumentos.ToUpper(); // MOEDAORIGEM=REAL&MOEDADESTINO=DOLAR
+
+            // Mesmo com a imutabilidade das strings, é possível fazer atribuições como:
+            // url += "sufixo"; que estaria criando uma string temporária com a união da url + "sufixo" e que depois estaria atribuindo a url o valor dessa variável temporária
+            string termo = nomeParametro + '='; // Recebe "moedaOrigem" ou "moedaDestino" e adiciona um "=" para encontrat o índice da string futuramente
+            int indiceTermo = argumentoEmCaixaAlta.IndexOf(termo); // pege o índice de "moedaOrigem=" ou "moedaDestino="
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length); // Retorna a substring que começa a partir do índice do termo + "=" somado ao valor do tamanho do próprio termo, sobrando apenas o valor "real" ou "dolar"
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if (indiceEComercial == -1)
+            {
+                // Caso o índice do & comercial não retorne nada (ou seja, se trata de uma "moedaDestino") apenas retorna o resultado, sem remover nada e evitando erros.
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial); 
+        }
+    }
+}
--- 2 - C#/7-C1-StringsExpressoesRegularesClasseObject/Program.cs
+++ 2 - C#/7-C1-StringsExpressoesRegularesClasseObject/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using _7_C1_StringsExpressoesRegularesClasseObject;
+using System.Text.RegularExpressions;
 
 namespace _7_C1_StringsExpressoesRegularesClasseObject
 {
@@ -11,13 +13,52 @@ namespace _7_C1_StringsExpressoesRegularesClasseObject
     {
         static void Main(string[] args)
         {
-            string url;
+            // Expressões Regulares:
+            string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]"; // Expressão regular que define o padrão de uma string referente a um número de telefone
+
+            string textoDeTeste = "Meu nome é Iago, me ligue em 1234-1234";
+
+            Console.WriteLine(Regex.IsMatch(textoDeTeste, padrao)); // Método da classe Regex (Regular expressions) que verifica se uma string qualquer combina com o padrão definido. Recebe como parâmetros a string a ser testada e o padrão, que deve ser formatado com: "[valores_permitidos][valores_permitidos][valores_permitidos]";
+
+            Match resultado = Regex.Match(textoDeTeste, padrao); // Ao invés de retorar apenas True ou False, retorna um objeto com as propriedades do texto que respeita o padrão informado.
+            Console.WriteLine(resultado.Value); // Retora o valor do objeto Match, no caso, o número.
+
+            Console.ReadLine();
+
+            // Testando o StartsWith, EndsWith e o Contains:
+            string urlTeste = "https://www.bytebank.com/cambio";
+            Console.WriteLine(urlTeste.StartsWith("https://www.bytebank.com")); // Verifica se a url se trata de um domínio do bytebank, ou seja, se a string começa com os mesmos caracteres indicados
+
+            Console.WriteLine(urlTeste.EndsWith("https://www.bytebank.com")); // Faz a mesma coisa que o método anterior, mas olhando para o final da string
+
+            Console.WriteLine(urlTeste.Contains("bytebank")); // Retorna true caso a string informada esteja contida na string de referência
+
+            Console.ReadLine();
+
+            // Usando a classe ExtratorValorDeArgumentosURL:
+            string _url;
             // Páginas usam formatações específicas para tratarem de páginas dinâmicas, por exemplo:
             // https://www.cptm.sp.gov.br/cptm/sua-viagem/para-onde-voce-vai?para=974&de=3151&acessibilidade=false
             // A "?" separa a porção da URL que contém a página dos argumentos passados
             // O "&" separa os argumentos entre si
-            url = "https://www.bytebank.com.br/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            _url = "https://www.bytebank.com.br/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+
+            string url = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
             Console.WriteLine(url);
+
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(url);
+
+            Console.WriteLine($"Valor da moeda de origem: {extratorDeValores.GetValor("moedaOrigem")}");
+            Console.WriteLine($"Valor da moeda destino: {extratorDeValores.GetValor("moedaDestino")}");
+            Console.WriteLine($"Valor: {extratorDeValores.GetValor("VALOR")}");
+
+            Console.ReadLine();
+
+            // Testando o método Remove():
+            //string testeRemocao = "primeiraParte&parteParaRemover";
+            //int indiceEComercial = testeRemocao.IndexOf('&');
+            //Console.WriteLine(testeRemocao.Remove(indiceEComercial, 4));
+            //Console.ReadLine();
         }
     }
 }
