eba3ca5561494606dd9f9af9c2152f65cbb8f1d5
IagoBGCarvalho <iagobgc@gmail.com>
2025-11-03 17:59:37 -0300

    Início do curso: Entity Framework Core: Banco de dados de forma eficiente, envolvendo a configuração inicial e a implementação completa da funções do CRUD

---
+++ 5 - Entity/1-EFCore-BancodeDadosdeFormaEficiente/Data/LojaContext.cs
@@ -0,0 +1,29 @@
+﻿using Alura.Loja.Testes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.Data
+{
+    internal class LojaContext : DbContext // Deve herdar da classe DbContext da API do Entity que contém os principais métodos para manipular um bd
+    {
+        // O contexto se trata da classe .NET que irá liderar a lógica de implementação, manipulação e manutenção do banco de dados.
+
+        // O contexto deve definir 3 coisas:
+
+        // 1 - Classes a serem persistidas:
+        public DbSet<Produto> Produtos { get; set; } // DbSet se trata da classe que faz o mapemanto de uma propriedade para uma tabela do banco de dados. Por isso, deve mapear uma classe que representa, diretamente, uma tabela do banco
+
+        // 2 - O banco de dados (qual é o banco de dados e onde ele fica):
+
+        // Para fazer isso, é necessário sobreescrever o método de configuração da classe DbContext:
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // O método recebe como parâmetro o construtor de opções, que pode ser utilizado para definir propriedades específicas do contexto.
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=LojaDB;Trusted_Connection=True;TrustServerCertificate=True"); // Utilizando as opções para definir que o SqlServer é o banco utilizado e também especificar a string de conexão, que deve conter o servidor e o nome do banco.
+        }
+    }
+}
---
+++ 5 - Entity/1-EFCore-BancodeDadosdeFormaEficiente/Program.cs
@@ -0,0 +1,105 @@
+﻿// O Microsoft Entity Framework Core é uma ferramenta multiplataforma utilizada para realizar operações em bancos de dados sem precisar formular todas os comandos SQL de manipulação de dados na mão
+
+// Script para criar a tabela utilizada:
+//CREATE TABLE[dbo].[Produtos] (
+//    [Id]        INT IDENTITY(1, 1) NOT NULL,
+//    [Nome]      NVARCHAR (MAX) NULL,
+//    [Categoria] NVARCHAR (MAX) NULL,
+//    [Preco]     FLOAT (53)     NULL,
+//    CONSTRAINT[PK_Produtos] PRIMARY KEY CLUSTERED ([Id] ASC)
+//);
+
+// Para utilizar o Entity em um projeto, é necessário instalar 3 pacotes (todos na versão 8.0.21 caso a versão do .NET seja a 8):
+// 1 - "Microsoft.EntityFrameworkCore" que contém o motor do Entity
+// 2 - "Microsoft.EntityFrameworkCore.SqlServer" que contém as injeções do SqlServer
+// 3 - "Microsoft.EntityFrameworkCore.Tools" que contém as ferramentas de migração
+
+// O entity permite fazer todas as operações CRUD (Create, Read, Update e Delete)
+
+using Alura.Loja.Testes.Data;
+using Alura.Loja.Testes.Models;
+
+namespace Alura.Loja.Testes {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GravarUsandoEntity();
+            //RecuperarProdutos();
+            //ExcluirProdutos();
+            //RecuperarProdutos();
+            //AtualizarProduto();
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            // Create (está criando um novo dado no sistema)
+
+            // Utiliza-se o modelo referente a tabela para criar uma variável que fará o mapeamento dos parâmetros para um ojeto que representará os dados gravados na tabela.
+            // Cada propriedade do objeto deve ser preenchida de acordo com as especificações das colunas da tabela
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            // O using é utilizado para ininicar a conexão, realizar os processamentos e depois encerra-lá utilizando o dispose():
+            using (var contexto = new LojaContext())
+            {
+                contexto.Produtos.Add(p); // Para dar um insert no banco, utiliza-se a propriedade que representa a tabela e o método Add que fará toda a formatação do insert automaticamente
+                contexto.SaveChanges(); // SaveChanges sempre deve ser utilizado para implementar as alterações
+            }
+        }
+
+        private static void RecuperarProdutos()
+        {
+            // Read (está lendo os dados já existentes no sistema)
+
+            using (var contexto = new LojaContext())
+            {
+                // Para retornar um SELECT, é necessário converter a propriedade que representa o banco em uma lista e
+                // depois atribuir o resultado a uma variável que implementa uma interface IList do modelo que
+                // representa a tabela.
+                IList<Produto> produtos = contexto.Produtos.ToList();
+                Console.WriteLine("\nForam encontrados {0} produto(s).", produtos.Count);
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
+        }
+
+        private static void AtualizarProduto()
+        {
+            // Update (está atualizando partes de um dado no sistema ou ele todo)
+
+            // Incluir um produto
+            GravarUsandoEntity(); // Gravando um produto para teste
+            RecuperarProdutos();
+
+            // Atualizar o produto
+            using (var contexto = new LojaContext())
+            {
+                Produto primeiro = contexto.Produtos.First(); // Para teste, pegando o primeiro produto, mas poderia ser outra condição
+                primeiro.Nome = "Harry Potter - Editado"; // Alterando a propriedade do objeto que representa a coluna do registro a ser alterada
+                contexto.Produtos.Update(primeiro); // Chama o Update do entity que automaticamente cria um comando SQL UPDATE com as alterações realizadas previamente no objeto
+                contexto.SaveChanges();
+            }
+            RecuperarProdutos();
+        }
+
+        private static void ExcluirProdutos()
+        {
+            // Delete (está removendo um dado do sistema)
+
+            using (var contexto = new LojaContext())
+            {
+                IList<Produto> produtos = contexto.Produtos.ToList();
+                foreach (var item in produtos)
+                {
+                    contexto.Produtos.Remove(item); // Utiliza os produtos encontrados pelo select para remover eles da propriedade do contexto, consequentemente, da tabela do banco de dados.
+                }
+                contexto.SaveChanges(); // É necessário salvar as mudanças depois que todas as remoções são feitas na propriedade
+            }
+        }
+    }
+}
