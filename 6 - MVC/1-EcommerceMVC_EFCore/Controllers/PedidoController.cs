b7c6062eb0ca96967ff36045e85d4d61050df2f2
IagoBGCarvalho <iagobgc@gmail.com>
2025-11-12 16:54:27 -0300

    Início do curso: ASP.NET Core e-Commerce com MVC e EF Core. Está com problemas relacionados à versão do dotnet, impossibilitando que o bootstrap funcione corretamente.

---
+++ 6 - MVC/1-EcommerceMVC_EFCore/Controllers/PedidoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller // Deve herdar da classe Controller do AspNet
+    {
+        // O controlador é a tecnologia intermediária que junta as views com os dados brutos (models) para gerar a página HTML.
+
+        // Para localizar o controlador, a action e a view, o ASP.NET pega o complemento da URL (o que vem depois do domínio e da porta) e procura no diretório
+        // do projeto por um arquivo com o nome do complemento (por exemplo: Home) e "Controller" logo em seguida (como: HomeController).
+
+        // Após isso, irá analisar a action, ou seja, a partícula que vem após o complemento da URL (como: .../home/privacy) e irá procurar por um método
+        // no controlador que corresponda a este nome. Este método deve ter uma IActionResult como devolução e deve retornar o método View() que irá acionar
+        // o arquivo cshtml com o mesmo nome do método.
+        private readonly ILogger<PedidoController> _logger;
+
+        public PedidoController(ILogger<PedidoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View(); // Retorna o arquivo cshtml com o nome do método, no caso "Carrossel.cshtml"
+        }
+    }
+}
