420af5dc439a9f5665431699832026b062c48cdc
IagoBGCarvalho <iagobgc@gmail.com>
2025-11-13 15:23:25 -0300

    Correção dos problemas relacionados a estilização da página, envolvendo o uso da biblioteca Library Manager para retirar os pacotes do bootstrap 5 do projeto e colocar os do 3.

--- 6 - MVC/1-EcommerceMVC_EFCore/Program.cs
+++ 6 - MVC/1-EcommerceMVC_EFCore/Program.cs
@@ -1,4 +1,37 @@
+// Como o curso foi ministrado com o .NET 6 e hoje em dia ele praticamente não é mais utilizado, iniciei uma jornada para conseguir aprender MVC,
+// mas utilizando a versão mais nova da tecnologia, pois ela possui algumas diferenças significativas, como a ausência do arquivo de Startup.
+
+// Criei um projeto MVC com .NET 8, copiei os arquivos de recurso do professor (.css, .cshtml, imagens, etc...) que foram criados em um projeto mais
+// antigo (.NET 6) e o resultado foi uma página quebrada, com elementos desalinhados, sem header/footer e sem carrossel, indicando que o HTML do
+// _Layout.cshtml estava sendo carregado, mas o CSS não.
+
+// Tentei refazer o projeto e copiar e colar novamente os arquivos, acreditando que tinha sido algum erro humano, mas nada adiantou. Ao analisar os
+// arquivos, notei que o arquivo de layout referenciava versões mais antigas do bootstrap e do jquery, como em: href="...bootstrap/3.3.7/..." e
+// src=".../jquery/jquery-2.2.0...". O modelo de MVC do .NET 8 cria uma pasta lib em wwwroot com os pacotes do bootstrap 5, gerando o conflito.
+
+// Para resolver isso, instalei uma biblioteca chamada libman (Library Manager) utilizando: "dotnet tool install -g Microsoft.Web.LibraryManager.Cli"
+// que permite substituir bibliotecas instaladas em um projeto por outras versões. Para substituir as libs do Bootstrap 5 pelo 3, criei o arquivo
+// "libman.json" na raiz do projeto e lá especifiquei as versões do Bootstrap e do Jquery que estavam expressas no arquivo de layout e também as
+// localizações de onde cada pasta/arquivo deveria ficar (levando em consideração as linhas do _Layout.cshtml que fazem a requisição desses arquivos).
+// Depois de criar o arquivo, apenas tive que apagar o diretório wwwroot/lib e dar um "libman restore" para criar novamente, mas com as especificações
+// do libman.json.
+
+// Após isso tudo, a página piorou, ficou sem estilização alguma (antes existia, mas era quebrada), o que me levou a outra investigação. Utilizei as
+// ferramentas de desenvolvedor do navegador, entrei na área de rede, desativei o cache e atualizei a página para verificar os arquivos que o navegador
+// estava tentando carregar, se ele conseguia e, se não (404) qual era o caminho pelo qual ele estava tentando requisitar esses arquivos. A primeira prova
+// foi que o navegador estava tentando solicitar os arquivos "bootstrap.min.css", "site.min.css" e "bootstrap.bundle.min.js" mas estava recebendo um 404. 
+
+// Ao dar um "tree wwwroot /f" no projeto, percebi que esses arquivos existiam (bootstrap.css e site.css), mas não em forma em forma minificada, já o
+// bootstrap.bundle é um arquivo que está presente apenas no bootstrap 4 para cima, não no baixado pelo libman (3). Ou seja, juntando todas as pistas,
+// cheguei a conclusão de que se tratava de uma maré de conflitos: o layout que o servidor estava usando era do .NET 8, então pedia os arquivos do
+// bootstrap 5, estava rodando em modo de produção (pois estava pedindo os arquivos .min) e o libman apenas tinha criado os arquivos normais.
+
+// Para resolver definitivamente, forcei o modo de desenvolvimento no Program.cs, entrei em um repositório qualquer do github que versionou o projeto
+// da aula para copiar o conteúdo do _Layout.cshtml e colar no meu e limpei o cache do servidor com "dotnet clean" e "dotnet build".
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Environment.EnvironmentName = Environments.Development; // Força o programa a rodar em modo de desenvolvimento
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
