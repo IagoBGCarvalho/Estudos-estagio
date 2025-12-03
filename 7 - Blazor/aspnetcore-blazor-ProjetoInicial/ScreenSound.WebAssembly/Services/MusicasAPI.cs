4fc9aaa928490efbe6d88ba77e4750b3c7cc8e7f
IagoBGCarvalho <iagobgc@gmail.com>
2025-12-03 16:58:10 -0300

    Finalização do curso, envolvendo a implementação de ícones do Bootstrap Icons para finalizar a página de Playlist.

--- 7 - Blazor/aspnetcore-blazor-ProjetoInicial/ScreenSound.WebAssembly/Services/MusicasAPI.cs
+++ 7 - Blazor/aspnetcore-blazor-ProjetoInicial/ScreenSound.WebAssembly/Services/MusicasAPI.cs
@@ -10,15 +10,30 @@ namespace ScreenSound.WebAssembly.Services
         // Classe responsável por fazer a comunicação com a API para obter dados relacionados as músicas
 
         private readonly HttpClient _httpClient;
+        private readonly ILogger<MusicasAPI> _logger;
 
-        public MusicasAPI(IHttpClientFactory factory)
+        public MusicasAPI(IHttpClientFactory factory, ILogger<MusicasAPI> logger)
         {
             _httpClient = factory.CreateClient("API");
+            _logger = logger;
         }
 
         public async Task<ICollection<MusicaResponse>> GetMusicaAsync()
         {
-            return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+            try
+            {
+                _logger.LogInformation("Tentando buscar músicas no endpoint de músicas.");
+                var result = await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+                _logger.LogInformation("Busca de músicas concluída.");
+                return result;
+            }
+            catch (Exception e)
+            {
+                string mensagem = "Erro ao buscar músicas da API";
+                _logger.LogError(e, mensagem); // Atribui a exceção completa e uma mensagem personalizada ao log
+                throw new ApplicationException(mensagem, e); // Atribui a mensagem personalizada e a exceção original ao lançamento da nova exceção
+            }
+            
         }
     }
 }
